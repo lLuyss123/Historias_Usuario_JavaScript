08674fa7905279e3f8ab63178510678bfe3fe349
luis <lmejiaidep@gmail.com>
2026-05-15 12:35:58 -0500

    hola3

--- semana2/gestion_datos.js
+++ semana2/gestion_datos.js
@@ -1,3 +1,5 @@
+//Creamos un arreglo de productos con sus claves y su valores
+
 const productos = [
     {
         "id": 1,
@@ -19,14 +21,26 @@ const productos = [
 ]
 
 
+console.log(productos);
+
+//creamos un set con datos repetidos pero como el set no acepta repetios el los elimina y se queda con un valór unico sin repetidos
 const repetidos = new Set([1, 2, 3, 4, 4, 4, 5])
 console.log(repetidos);
+// usamos el add para agregar en un set 
 repetidos.add(4)
+// usamos el metodo delete para borrar un dato del set
 repetidos.delete(1)
+// usamos el has para verificar si el set tiene el valor dentro del arreglo
 console.log(repetidos.has(4));
+// usamos un for of para recorrer el arreglo Set
 for (const element of repetidos) {
     console.log(element);
 }
 
 
+console.log("MAPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPP");
+
+const arreglo = new Map();
+arreglo.set("clave1", 1)
+console.log(arreglo);
 
